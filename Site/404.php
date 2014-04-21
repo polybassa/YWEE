07776fbd70ecb5203bbc9c553d6a66f8bb05100f
daniel <daniel@Thor>
2014-04-21 20:58:47 +0200

    Einige DB Fixes

--- Site/404.php
+++ Site/404.php
@@ -1,22 +1,23 @@
 <?php
-// Anpassung und Aufteilung des Layouts: Daniel Tatzel
-// Muss in der Reihenfolge bleiben
-include_once($_SERVER["DOCUMENT_ROOT"] . "/scripts/ConToDB.php");       // Inkludiert die Funktion zur Anmeldung an der DB
-include_once($_SERVER["DOCUMENT_ROOT"] . "/scripts/StaticDBFuncs.php"); // Inkludiert statische DB Funktionen
-include_once($_SERVER["DOCUMENT_ROOT"] . "/scripts/counter.php");       // Inkludiert den Counter Script
-include_once($_SERVER["DOCUMENT_ROOT"] . "/scripts/login.php");         // Inkludiert das Login Script. Bleibt vorerst so, muesste mit AJAX implementiert werden
+    // Anpassung und Aufteilung des Layouts: Daniel Tatzel
+    // Muss in der Reihenfolge bleiben
+    include_once($_SERVER["DOCUMENT_ROOT"] . "/scripts/ConToDB.php");       // Inkludiert die Funktion zur Anmeldung an der DB
+    include_once($_SERVER["DOCUMENT_ROOT"] . "/scripts/StaticDBFuncs.php"); // Inkludiert statische DB Funktionen
+    include_once($_SERVER["DOCUMENT_ROOT"] . "/scripts/counter.php");       // Inkludiert den Counter Script
+    include_once($_SERVER["DOCUMENT_ROOT"] . "/scripts/login.php");         // Inkludiert das Login Script. Bleibt vorerst so, muesste mit AJAX implementiert werden
 
-$titel = "Die Seite konnte nicht gefunden werden"; // Name der Seite die im Browser angezeigt werden soll
+    $titel = "Die Seite konnte nicht gefunden werden"; // Name der Seite die im Browser angezeigt werden soll
+    $StatusCodeError = 1;   // Wird (nur!) hier fuer Login und -out benoetigt (Script wird wegen dem Rewrite nicht ausgefuehrt)
 
-include("$_SERVER[DOCUMENT_ROOT]/layout/header.php");   // Inkludiert den Header
-include("$_SERVER[DOCUMENT_ROOT]/layout/nav.php");      // Inkludiert die Navigationsleiste
-include("$_SERVER[DOCUMENT_ROOT]/layout/leftbar.php");  // Inkludiert den linken Balken
-include("$_SERVER[DOCUMENT_ROOT]/layout/rightbar.php"); // Inkludiert den rechten Balken
+    include("$_SERVER[DOCUMENT_ROOT]/layout/header.php");   // Inkludiert den Header
+    include("$_SERVER[DOCUMENT_ROOT]/layout/nav.php");      // Inkludiert die Navigationsleiste
+    include("$_SERVER[DOCUMENT_ROOT]/layout/leftbar.php");  // Inkludiert den linken Balken
+    include("$_SERVER[DOCUMENT_ROOT]/layout/rightbar.php"); // Inkludiert den rechten Balken
 
-// $_POST Debug
-//print_r($_POST);
+    
+    //print_r($_POST); // Debug Ausgabe fuer den Inhalt von $_POST
 
-// Inhalt von: Daniel Tatzel
+    // Inhalt von: Daniel Tatzel
 ?>
 
 <h2>Fehler</h2>
--- Site/layout/footer.php
+++ Site/layout/footer.php
@@ -1,7 +1,7 @@
     </div> <!-- ID: Content -->
     
     <div id="footer">
-        Footer Inhalt!
+        Footer Inhalt! <a href="phpinfo.php">PHP Info</a>
     </div>
 </div> <!-- ID: Container -->
 </body>
