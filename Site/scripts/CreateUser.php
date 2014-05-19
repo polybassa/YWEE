93a9d3c115c15dcb0fe0bf4f7b02596faa1d8484
IOIOI <mail@tatzel.cc>
2014-05-19 23:05:28 +0200

    DB Anpassung und Script Erweiterung

--- Site/scripts/CreateUser.php
+++ Site/scripts/CreateUser.php
@@ -3,23 +3,23 @@
     // Erstellt einen neuen Benutzer in der Datenbank
     if ( $_POST['username'] != 'Benutzername')
     {
+        //include($_SERVER["DOCUMENT_ROOT"] . "/scripts/ConToDB.php");
+
         // Baue Verbindung auf
         $dbConnection = ConnectToDB();
             
         $dbConnection->setAttribute(PDO::ATTR_CASE, PDO::CASE_NATURAL);
 
-        $query = $dbConnection->prepare("insert into mitglieder (benutzername, geschlecht, vorname, nachname, geburtsdatum, wohnort, strasse, hausnummer, hnrzusatz, email, telefon, handy, sprache, rolle) VALUES (:user, :geschlecht, :vorname, :nachname, :geburtsdatum, :plz. :wohnort, :strasse, :hausnummer, :hnrzusatz, :email, :telefon, :sprache, :rolle);");
+        $query = $dbConnection->prepare("insert into mitglieder (benutzername, geschlecht, vorname, nachname, geburtsdatum, plz, wohnort, strasse, hausnummer, hnrzusatz, email, telefon, sprache) VALUES (:user, :geschlecht, :vorname, :nachname, :geburtsdatum, :plz, :wohnort, :strasse, :hausnummer, :hnrzusatz, :email, :telefon, :sprache);");
         
         $query->bindParam(":user",$_POST['username']);
         
-        if ( $_POST['geschlecht'] == "M")
-            $query->bindParam(":geschlecht", 1);
-        else
-            $query->bindParam(":geschlecht", 0);
+        $query->bindParam(":geschlecht", $_POST['geschlecht']);
             
         $query->bindParam( ":vorname" ,$_POST['vorname'] );
         $query->bindParam( ":nachname", $_POST['nachname'] );
-        $query->bindParam( ":geburtsdatum", $_POST['gebjahr'].$_POST['gebmonat'].$_POST['gebtag'] );
+        $GebDatum = $_POST['gebjahr'].$_POST['gebmonat'].$_POST['gebtag'];
+        $query->bindParam( ":geburtsdatum", $GebDatum );
         $query->bindParam( ":plz", $_POST['plz'] );
         $query->bindParam( ":wohnort", $_POST['wohnort'] );
         $query->bindParam( ":strasse", $_POST['strasse'] );
@@ -34,7 +34,7 @@
         $query->bindParam( ":telefon", $_POST['telefon'] );
 
         $query->bindParam( ":sprache", $_POST['sprache'] );
-        
+
         $query->execute();
 
         $query = $dbConnection->prepare( "insert into login (benutzername, passwort, rolle) VALUES ( :user, :pass, 0)" );
