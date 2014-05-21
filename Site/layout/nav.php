effb52f890ed5acd01cce3497351b788b34a6fe0
IOIOI <mail@tatzel.cc>
2014-05-21 20:59:00 +0200

    Testlinks in Nav + Registrierung

--- Site/layout/nav.php
+++ Site/layout/nav.php
@@ -1,3 +1,4 @@
     <div id="nav" >
-        Navigation: Home News Unternehmen Kontakt
+        Navigation: Home News Unternehmen Kontakt <br>
+        Links zu aktuellen Seiten: <a href="/index.php">Startseite</a> &nbsp; &nbsp; <a href="/impressum.php">Impressum</a> &nbsp; &nbsp; <a href="/registrierung.php">Registrierung</a>
     </div>
