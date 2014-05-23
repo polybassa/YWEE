5aee4225b88389ca30e397bab4bb7e66f09f6481
IOIOI <mail@tatzel.cc>
2014-05-23 23:34:38 +0200

    Kommentare und Anpassungen

---
+++ Site/en/news.php
@@ -0,0 +1,19 @@
+<?php
+    // Anpassung und Aufteilung des Layouts: Daniel Tatzel
+    // Muss in der Reihenfolge bleiben
+    include_once($_SERVER["DOCUMENT_ROOT"] . "/test_02/scripts/session.php");       // Inkludiert die Session
+
+    $titel = "News"; // Name der Seite die im Browser angezeigt werden soll
+
+    $_SESSION['sprache'] = "en";
+    
+    include($_SERVER["DOCUMENT_ROOT"] . "/test_02/layout/header.php");   // Inkludiert den Header
+    
+    //print_r($_POST); // Debug Ausgabe fuer den Inhalt von $_POST
+    //echo nl2br(print_r($_SESSION,true));  // Debug Ausgabe fuer Session
+
+    include_once($_SERVER["DOCUMENT_ROOT"] . "/test_02/en/content/news.html");       // Inkludiert den Inhalt
+        
+
+    include($_SERVER["DOCUMENT_ROOT"] . "/test_02/layout/footer.php"); // Inkludiert den Footer
+?>
