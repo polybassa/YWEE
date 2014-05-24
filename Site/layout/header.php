cf5d54880cad5fe2c7a8a9fe6d2588d03d924064
IOIOI <mail@tatzel.cc>
2014-05-24 19:07:17 +0200

    Kleien fixes

--- Site/layout/header.php
+++ Site/layout/header.php
@@ -15,14 +15,10 @@
 <div id="container">
 
     <div id="banner" >
-        <!-- <a href="http://www.intensivstation.ch"><img src="http://www.intensivstation.ch/files/images/monorom_css_logo.gif" alt="" width="414" height="56" border="0" /></a>
-            <h1>service for a better code</h1>
-        -->
-        Banner (insgesamt 900 Pixel breit): Links das Logo, danaben Slideshow
+        Banner: Links das Logo, danaben Slideshow
     </div>
 
 <?php
-
     include($_SERVER["DOCUMENT_ROOT"] . "/test_02/layout/nav.php");      // Inkludiert die Navigationsleist
     include($_SERVER["DOCUMENT_ROOT"] . "/test_02/layout/leftbar.php");  // Inkludiert den linken Balken
     include($_SERVER["DOCUMENT_ROOT"] . "/test_02/layout/rightbar.php"); // Inkludiert den rechten Balken
