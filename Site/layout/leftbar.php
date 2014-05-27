3ff7be2b2df6c634fcce7f90896bd099d7f69778
Birni <Matthias-Birnthaler@hotmail.de>
2014-05-27 22:59:31 +0200

    #leftbar.php -> layout fürs Gästebuch

--- Site/layout/leftbar.php
+++ Site/layout/leftbar.php
@@ -22,5 +22,75 @@
             include_once($_SERVER["DOCUMENT_ROOT"] . "/test_02/scripts/GetCounterValue.php"); // Inkludiert die Counter Abfrage
         }
     ?>
-    </p>
+    
+	<!----------Block for Map by Matthias Birnthaler ----------------->
+	<?php
+	
+	if ( $_SESSION['sprache'] == "en" )
+            {
+                echo '<div class="basic-wrapper-top">
+						Guestbook </div>';      
+            }
+            else
+            {
+                echo '<div class="basic-wrapper-top">
+						Gästebuch</div>';
+			}
+	?>
+	 
+	<div class="basic-wrapper-middle">
+	<br>
+	placeholder für Gästebuch
+	<br>
+	<br>
+	<hr class="lane">
+	</div>
+	
+	
+	<div class="basic-wrapper-middle">
+	<br>
+	Inhalt steht zum Anschaun im leftbar.php (uncool) 
+	<br>
+	<br>
+	<hr class="lane">
+	</div>
+	
+	<div class="basic-wrapper-middle">
+	<br>
+	des kann muss noch irgendwer hinbiegen
+	<br>
+	<br>
+	<hr class="lane">
+	</div>
+	
+	<div class="basic-wrapper-middle"> <!-- middle item-->
+	<br>
+	ich nicht ich bin fürs layout da 
+	<br>
+	<br>
+	<hr class="lane">
+	</div>
+	
+	<div class="basic-wrapper-middle">
+	<br>
+	nutz mein layout 
+	<br>
+	<br>
+	<hr class="lane">
+	</div>
+	
+	<div class="basic-wrapper-bottom"> <!--last item- no lane for the last item-->
+	<br>
+	ich würde nicht läger gehen als die rechte spalte (scrollbalken oder max Einträge!) 
+	</div
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	</p>
 </div>
