1bccffc8d7501cf08716b2148ca41e0d2589e4a3
Litschi <tobias.schwindl@live.de>
2014-05-29 16:08:03 +0200

    News schreiben für den Admin

---
+++ Site/de/news.php
@@ -0,0 +1,16 @@
+<?php
+	include_once($_SERVER["DOCUMENT_ROOT"] . "/test_02/scripts/session.php");       // Inkludiert die Session
+	//$_SESSION['user'] = "admin";
+	
+	$titel = "News"; // Name der Seite die im Browser angezeigt werden soll
+	
+	 $_SESSION['sprache'] = "de";
+	 
+	include($_SERVER["DOCUMENT_ROOT"] . "/test_02/layout/header.php");   // Inkludiert den Header
+	 
+	if ( $_SESSION['user'] == "admin" )
+	{		
+		include_once($_SERVER["DOCUMENT_ROOT"] . "/test_02/de/content/news.html");       // Inkludiert den Inhalt
+	}
+	include($_SERVER["DOCUMENT_ROOT"] . "/test_02/layout/footer.php"); // Inkludiert den Footer
+?>
---
+++ Site/scripts/WriteNews.php
@@ -0,0 +1,38 @@
+<?php
+    //News schreiben, für den Admin, Tobias Schwindl
+
+    include_once($_SERVER["DOCUMENT_ROOT"] . "/test_02/scripts/ConToDB.php");       // Inkludiert die Funktion zur Anmeldung an der DB
+    // Baue Verbindung auf
+    try {
+        $dbConnection = ConnectToDB();
+    } catch (Exception $e) {
+        die("keine Verbindung möglich: " . $e->getMessage());
+    }
+	
+    // SECURITY HOLE ***************************************************************
+    // allow space, any unicode letter and digit, underscore and dash
+    if ( preg_match("/[^\040\pL\pN_-]/u", $_POST['nachricht']) || preg_match("/[^\040\pL\pN_-]/u", $_POST['subject'])) {
+        exit;
+    }
+    
+    // replace multiple spaces with one
+    $user = preg_replace('/\s+/', ' ', $_SESSION['user']);
+    $msg = preg_replace('/\s+/', ' ', $_POST['nachricht']);
+	$subject = preg_replace('/\s+/', ' ', $_POST['subject']);
+    
+    // Set the case in which to return column_names.
+    $dbConnection->setAttribute(PDO::ATTR_CASE, PDO::CASE_NATURAL);
+	
+    $query = $dbConnection->prepare("insert into news (benutzername, zeit, nachricht, betreff) VALUES ( :user, CURRENT_TIMESTAMP, :msg, :subject)");
+
+    $query->bindParam( ":msg", $msg );
+	  $query->bindParam( ":user", $user );
+	$query->bindParam( ":subject", $subject);
+    
+    $query->execute();
+	
+	print_r($_POST);
+	
+    $dbConnection = null;
+?> 
+
