d7950fe80feb15d212970bb5afd4d32fa80af1b2
Litschi <tobias.schwindl@live.de>
2014-05-31 13:50:34 +0200

    Update News

--- Site/de/nachrichten.php
+++ Site/de/nachrichten.php
@@ -2,7 +2,7 @@
     // Anpassung und Aufteilung des Layouts: Daniel Tatzel
     // Muss in der Reihenfolge bleiben
     include_once($_SERVER["DOCUMENT_ROOT"] . "/test_02/scripts/session.php");       // Inkludiert die Session
-
+	$lang = $_SESSION['sprache'];
     $titel = "Nachrichten"; // Name der Seite die im Browser angezeigt werden soll
 
     $_SESSION['sprache'] = "de";
@@ -12,7 +12,22 @@
     //print_r($_POST); // Debug Ausgabe fuer den Inhalt von $_POST
     //echo nl2br(print_r($_SESSION,true));  // Debug Ausgabe fuer Session
 
-    include_once($_SERVER["DOCUMENT_ROOT"] . "/test_02/de/content/nachrichten.html");       // Inkludiert den Inhalt
-        
-    include($_SERVER["DOCUMENT_ROOT"] . "/test_02/layout/footer.php"); // Inkludiert den Footer
+   if( $_SESSION['admin'] == true )
+	{
+?>
+
+	<form name="News" method="post" action="/test_02/scripts/WriteNews.php" id="newsform">
+		<div><label for ="betreff"></label><input type="text" size="44" maxlength = "60" placeholder = "Betreff" name="subject" id="betreff"required></div>
+		<div><label for ="nachrichtentext"></label>
+		<p><textarea name="nachricht" cols="33" rows="5" maxlength="300" placeholder ="Schreiben Sie hier ihre Nachricht..." required></textarea>
+		</p>
+		</div>
+		<div class="line submit"><input type="submit" value="Absenden"></div>
+	</form>
+
+<?php
+	}
+	include($_SERVER["DOCUMENT_ROOT"] . "/test_02/de/content/Nachrichten.html");
+	include($_SERVER["DOCUMENT_ROOT"] . "/test_02/layout/footer.php"); // Inkludiert den Footer
+	header("Location: http://ebenezer-kunatse.net/$lang/news.php");
 ?>
--- Site/en/news.php
+++ Site/en/news.php
@@ -11,7 +11,20 @@
     
     //print_r($_POST); // Debug Ausgabe fuer den Inhalt von $_POST
     //echo nl2br(print_r($_SESSION,true));  // Debug Ausgabe fuer Session
+	if( $_SESSION['admin'] == true )
+	{
+?>
+	<form name="News" method="post" action="/test_02/scripts/WriteNews.php" id="newsform">
+		<div><label for ="betreff"></label><input type="text" size="44" maxlength = "60" placeholder = "Subject" name="subject" id="betreff"required></div>
+		<div><label for ="nachrichtentext"></label>
+		<p><textarea name="nachricht" cols="33" rows="5" maxlength="300" placeholder ="Write here your message" required></textarea>
+		</p>
+		</div>
+		<div class="line submit"><input type="submit" value="Send"></div>
+	</form>
 
+<?php
+	}
     include_once($_SERVER["DOCUMENT_ROOT"] . "/test_02/en/content/news.html");       // Inkludiert den Inhalt
         
 
