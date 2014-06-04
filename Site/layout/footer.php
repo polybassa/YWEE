3331f90d0edefd5a8293c513b728d20e98a4cf32
Birni <Matthias-Birnthaler@hotmail.de>
2014-06-04 12:56:57 +0200

    stuff for footer

--- Site/layout/footer.php
+++ Site/layout/footer.php
@@ -2,8 +2,33 @@
 	</section> <!--ehemals wrapper-->
     
     <footer>
-        Footer Inhalt! <a href="/phpinfo.php">PHP Info</a>
-    </footer>
+		<?php
+		if ( $_SESSION['sprache'] == "en" )
+        { ?> 	
+		<div id="cssmenu">		
+				
+				<ul>
+					<li><a href="/en/termsandconditions.php"><span>Terms and Conditions</span></a></li>
+					<li><a href="/en/sitenotice.php"><span>Site notice</span></a></li>
+				</ul>
+		</div>
+		<?php
+		}
+        else
+        { ?>
+		<div id="cssmenu">
+				<ul>
+					<li><a href="/de/agb.php"><span>AGB</span></a></li>
+					<li><a href="/de/impressum.php"><span>Impressum</span></a></li>
+				</ul>
+		
+		</div>
+		<?php
+		}
+        ?>
+		
+		</div>
+	</footer>
 
 </div> <!-- ID: Container -->
 </body>
