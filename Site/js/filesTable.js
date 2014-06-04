29112d087229a00eb4133cbc917af25f3d001da5
Nils Weiß <n125.we155@googlemail.com>
2014-06-04 14:24:22 +0200

    language support of german in filestable.js

--- Site/js/filesTable.js
+++ Site/js/filesTable.js
@@ -7,7 +7,17 @@
 $(document).ready(function() {
 
 
-    var mTable = $('#filesTable').dataTable();
+    var mTable = $('#filesTable').dataTable({
+        "oLanguage": {
+            "sSearch": "Suchergebnisse filtern:",
+            "oPaginate": {
+                "sNext": "Nächste Seite",
+                "sPrevious": "Vorherige Seite"
+            },
+            "sInfo": "Es wurden _TOTAL_ Suchergebnisse gefunden. Aktuelle Anzeige (_START_ bis _END_)",
+            "sLengthMenu": "Zeige _MENU_ Suchergebnisse"
+        }
+    });
     mTable.fnClearTable();
     jQuery.each(files, function() {
         mTable.fnAddData(['<a href="files/' + this.name + '" > ' + this.name + '</a>', this.size, '<form method="POST" action=""><input type="hidden" name="file" value="'+ this.name +'"><input type="submit" name="delete" value="L&ouml;schen"></form>']);
