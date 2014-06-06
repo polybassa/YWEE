f5ae39c6b1e8a692220764f8afc8d1be30946141
Maxi Schröter <maxi_S15@yahoo.de>
2014-06-06 10:12:44 +0200

    no message

--- Site/js/registration.js
+++ Site/js/registration.js
@@ -74,7 +74,7 @@ $(document).ready(function()
 
                                     }
                                 });
-                        form.submit();
+                        //form.submit();
                     }
                 });
             });
@@ -170,7 +170,7 @@ $(document).ready(function()
 
                                     }
                                 });
-                        form.submit();
+                        //form.submit();
                     }
                 });
             });
