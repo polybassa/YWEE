cf47a2672196bfa5c620646da08548d5dc863973
Nils Weiß <n125.we155@googlemail.com>
2014-06-11 14:17:48 +0200

    change contentbox-size from guestbook

--- Site/de/gaestebuch.php
+++ Site/de/gaestebuch.php
@@ -1,43 +1,47 @@
 <?php
-    // Anpassung und Aufteilung des Layouts: Daniel Tatzel
-    // Muss in der Reihenfolge bleiben
-	// Angepasst von Alexander Strobl
-    include_once($_SERVER["DOCUMENT_ROOT"] . "/test_02/scripts/session.php");       // Inkludiert die Session
+// Anpassung und Aufteilung des Layouts: Daniel Tatzel
+// Muss in der Reihenfolge bleiben
+// Angepasst von Alexander Strobl
+include_once($_SERVER["DOCUMENT_ROOT"] . "/test_02/scripts/session.php");       // Inkludiert die Session
 
-    $titel = "G&auml;stebuch"; // Name der Seite die im Browser angezeigt werden soll
+$titel = "G&auml;stebuch"; // Name der Seite die im Browser angezeigt werden soll
 
-    $_SESSION['sprache'] = "de";
-    
-    include($_SERVER["DOCUMENT_ROOT"] . "/test_02/layout/header.php");   // Inkludiert den Header
-    
-	if( isset($_SESSION['logged-in']))
-	{
-?>
-	<form name="addguestbookentry" method="post" action="/scripts/WriteGuestbook.php" id="gbform">
-		<input type="hidden" name="sprache" value="<?php echo $_SESSION['sprache']; ?>">
-		<input type="hidden" name="username" value="<?php echo $_SESSION['user']; ?>">
-		<div>
-			<label for ="nachrichtentext"></label>
-			<p>
-				<textarea name="eintrag" cols="33" rows="5" maxlength="300" id="nachrichtentext" placeholder ="Schreiben Sie hier ihren Gästebucheintrag..." required></textarea>
-			</p>
-		</div>
-		<div class="line submit">
-			<input type="submit" value="Absenden">
-		</div>
-	</form>
+$_SESSION['sprache'] = "de";
 
-<?php	
-	}
-	else
-	{
-	?>
-		Nur eingeloggte User k&ouml;nnen G&auml;stebucheintr&auml;ge verfassen!
-		<br>
-	<?php	
-	}
-	// Inkludiert den Inhalt    
-	include_once($_SERVER["DOCUMENT_ROOT"] . "/test_02/de/content/gaestebuch.html"); 
-	        
-    include($_SERVER["DOCUMENT_ROOT"] . "/test_02/layout/footer.php"); // Inkludiert den Footer
+include($_SERVER["DOCUMENT_ROOT"] . "/test_02/layout/header.php");   // Inkludiert den Header
+
+if (isset($_SESSION['logged-in'])) {
+    ?>
+    <div style="height:150px;">
+        <form name="addguestbookentry" method="post" action="/scripts/WriteGuestbook.php" id="gbform">
+            <input type="hidden" name="sprache" value="<?php echo $_SESSION['sprache']; ?>">
+            <input type="hidden" name="username" value="<?php echo $_SESSION['user']; ?>">
+            <div>
+                <label for ="nachrichtentext"></label>
+                <p>
+                    <textarea name="eintrag" cols="33" rows="5" maxlength="300" id="nachrichtentext" placeholder ="Schreiben Sie hier ihren Gästebucheintrag..." required></textarea>
+                </p>
+            </div>
+            <div class="line submit">
+                <input type="submit" value="Absenden">
+            </div>
+        </form>
+    </div>
+    <script>
+        $(document).ready(function() {
+            $("#scrollablecontentboxguestebuch_de").height(600);
+        });
+    </script>
+
+    <?php
+} else {
+    ?>
+    Nur eingeloggte User k&ouml;nnen G&auml;stebucheintr&auml;ge verfassen!
+    <br>
+    <?php
+}
+// Inkludiert den Inhalt    
+include_once($_SERVER["DOCUMENT_ROOT"] . "/test_02/de/content/gaestebuch.html");
+
+include($_SERVER["DOCUMENT_ROOT"] . "/test_02/layout/footer.php"); // Inkludiert den Footer
 ?>
--- Site/en/guestbook.php
+++ Site/en/guestbook.php
@@ -1,42 +1,47 @@
 <?php
-    // Anpassung und Aufteilung des Layouts: Daniel Tatzel
-    // Muss in der Reihenfolge bleiben
-    include_once($_SERVER["DOCUMENT_ROOT"] . "/test_02/scripts/session.php");       // Inkludiert die Session
+// Anpassung und Aufteilung des Layouts: Daniel Tatzel
+// Muss in der Reihenfolge bleiben
+include_once($_SERVER["DOCUMENT_ROOT"] . "/test_02/scripts/session.php");       // Inkludiert die Session
 
-    $titel = "Guestbook"; // Name der Seite die im Browser angezeigt werden soll
+$titel = "Guestbook"; // Name der Seite die im Browser angezeigt werden soll
 
-    $_SESSION['sprache'] = "en";
-    
-    include($_SERVER["DOCUMENT_ROOT"] . "/test_02/layout/header.php");   // Inkludiert den Header
-    
-    if( isset($_SESSION['logged-in']))
-	{
-?>
-	<form name="addguestbookentry" method="post" action="/scripts/WriteGuestbook.php" id="gbform">
-		<input type="hidden" name="sprache" value="<?php echo $_SESSION['sprache']; ?>">
-		<input type="hidden" name="username" value="<?php echo $_SESSION['user']; ?>">
-		<div>
-			<label for ="nachrichtentext"></label>
-			<p>
-				<textarea name="eintrag" cols="33" rows="5" maxlength="300" id="nachrichtentext" placeholder ="Please write your guestbookentry here..." required></textarea>
-			</p>
-		</div>
-		<div class="line submit">
-			<input type="submit" value="send">
-		</div>
-	</form>
+$_SESSION['sprache'] = "en";
 
-<?php	
-	}
-	else
-	{
-	?>
-		Only logged-in users can add entries!
-		<br>
-	<?php	
-	}
+include($_SERVER["DOCUMENT_ROOT"] . "/test_02/layout/header.php");   // Inkludiert den Header
 
-    include_once($_SERVER["DOCUMENT_ROOT"] . "/test_02/en/content/guestbook.html");       // Inkludiert den Inhalt
-        
-    include($_SERVER["DOCUMENT_ROOT"] . "/test_02/layout/footer.php"); // Inkludiert den Footer
+if (isset($_SESSION['logged-in'])) {
+    ?>
+    <div style="height:150px;">
+        <form name="addguestbookentry" method="post" action="/scripts/WriteGuestbook.php" id="gbform">
+            <input type="hidden" name="sprache" value="<?php echo $_SESSION['sprache']; ?>">
+            <input type="hidden" name="username" value="<?php echo $_SESSION['user']; ?>">
+            <div>
+                <label for ="nachrichtentext"></label>
+                <p>
+                    <textarea name="eintrag" cols="33" rows="5" maxlength="300" id="nachrichtentext" placeholder ="Please write your guestbookentry here..." required></textarea>
+                </p>
+            </div>
+            <div class="line submit">
+                <input type="submit" value="send">
+            </div>
+        </form>
+    </div>
+
+    <script>
+        $(document).ready(function() {
+            $("#scrollablecontentboxguestebuch_en").height(600);
+        });
+    </script>
+
+    <?php
+} else {
+    ?>
+    Only logged-in users can add entries!
+    <br>
+    <?php
+}
+
+include_once($_SERVER["DOCUMENT_ROOT"] . "/test_02/en/content/guestbook.html");       // Inkludiert den Inhalt
+
+include($_SERVER["DOCUMENT_ROOT"] . "/test_02/layout/footer.php"); // Inkludiert den Footer
 ?>
